4924a2a8eb1e9c4ebde01a4d71ddb0f244da1618
canhtrinh <canh.coding@gmail.com>
2016-11-03 11:25:02 -0400

    feat: added basic socket.io functionality; configured passwords.env file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 var express = require('express');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // require middleware
 require('./config/middleware.js')(app, express);
 
+
+// For Socket.io, hacky approach
+require('../node_modules/socket.io-client/socket.io.js');
+
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
 //set and run the port and server
 app.set('port',process.env.PORT || 8000);
 var port = app.get('port');
-app.listen(port);
+http.listen(port);
 console.log("Server listening on PORT",port);
+
+
+
 
 //export app
 module.exports = app;
