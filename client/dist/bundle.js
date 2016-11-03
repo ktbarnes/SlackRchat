cf250563c0eff7f3f5634b70bc7b09d52cf094f0
canhtrinh <canh.coding@gmail.com>
2016-11-03 19:24:48 -0400

    feat: configured webpack to work

---
+++ client/dist/bundle.js
@@ -0,0 +1,66 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var socket = io();
+	$('form').submit(function () {
+	  socket.emit('chat message', $('#m').val());
+	  $('#m').val('');
+	  return false;
+	});
+	socket.on('chat message', function (msg) {
+	  $('#messages').append($('<li>').text(msg));
+	});
+	socket.on('disconnect', function () {
+	  console.log('a user has left.... aw man');
+	  $('#messages').append($('<li>').text('a user has left the room'));
+	});
+
+	console.log('Hello World! Why are you so awesome?');
+
+/***/ }
+/******/ ]);
