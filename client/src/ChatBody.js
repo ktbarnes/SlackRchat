3cf6c9b3361e3b52020ca01bfdaafb33f32159ef
canhtrinh <canh.coding@gmail.com>
2016-11-05 17:29:17 -0400

    feat: refactored client into React. still working on socket functionality to be incorporated as well

---
+++ client/src/ChatBody.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatBody = (props) => (
+
+  <div className="messagesList">
+    <ul id="messages"></ul>
+  </div>
+
+);
+
+export default ChatBody
--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,73 @@
-var socket = io();
-$('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
-});
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
-});
-socket.on('disconnect', function(){
-  console.log('a user has left.... aw man');
-  $('#messages').append($('<li>').text('a user has left the room'));
-});
+// var socket = io();
+// $('form').submit(function(){
+//   socket.emit('chat message', $('#m').val());
+//   $('#m').val('');
+//   return false;
+// });
+// socket.on('chat message', function(msg){
+//   $('#messages').append($('<li>').text(msg));
+// });
+// socket.on('disconnect', function(){
+//   console.log('a user has left.... aw man');
+//   $('#messages').append($('<li>').text('a user has left the room'));
+// });
       
-console.log('Hello World! Why are you so awesome?');
+// console.log('Hello World! Why are you so awesome?');
+
+import React from 'react';
+import ChatForm from './chatForm.js';
+import ChatBody from './ChatBody.js';
+
+
+class PrimaryChatroom extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // videos: [],
+      // currentVideo: null
+    };
+  }
+
+  // componentDidMount() {
+  //   // this.getYouTubeVideos('react tutorials');
+  // }
+
+  // getYouTubeVideos(query) {
+  //   var options = {
+  //     key: this.props.API_KEY,
+  //     query: query
+  //   };
+
+  //   this.props.searchYouTube(options, (videos) =>
+  //     this.setState({
+  //       videos: videos,
+  //       currentVideo: videos[0]
+  //     })
+  //   );
+  // }
+
+  // handleVideoListEntryTitleClick(video) {
+  //   this.setState({
+  //     currentVideo: video
+  //   });
+  // }
+
+  render() {
+    return (
+      <div>
+
+        <div>
+          <ChatBody />
+        </div>
+
+        <ChatForm
+          // handleSearchInputChange={this.getYouTubeVideos.bind(this)}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default PrimaryChatroom
