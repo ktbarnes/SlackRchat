3ebf5791a1721d638417ea3ffbf0223cfcfa6dbb
canhtrinh <canh.coding@gmail.com>
2016-11-07 16:02:45 -0500

    feat: added "disconnect" socket listener, but still buggy
    feature i want to add is to broadcast to all users that a user has left. this feature is still buggy
    
    will need to revisit componentdidmount in app.js to fix this issue

--- client/src/app.js
+++ client/src/app.js
@@ -18,8 +18,10 @@ class PrimaryChatroom extends React.Component {
     socket.on('chat message', function(message){
       that.handleReceiveMessage(message);
     });
+    socket.on('disconnect', function(message){
+      that.handleReceiveMessage(message);
+    });
   }
-
 
   handleReceiveMessage(chat) {
     let newChat = this.state.currentChat.concat(chat);
@@ -55,20 +57,3 @@ class PrimaryChatroom extends React.Component {
 }
 
 export default PrimaryChatroom
-
-//GUTTER: DO NOT USE THIS STUFF BELOW. FOR REFERENCE ONLY
-// var socket = io();
-// $('form').submit(function(){
-//   socket.emit('chat message', $('#m').val());
-//   $('#m').val('');
-//   return false;
-// });
-// socket.on('chat message', function(msg){
-//   $('#messages').append($('<li>').text(msg));
-// });
-// socket.on('disconnect', function(){
-//   console.log('a user has left.... aw man');
-//   $('#messages').append($('<li>').text('a user has left the room'));
-// });
-      
-// console.log('Hello World! Why are you so awesome?');
