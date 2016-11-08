0a7dbc4352a2475ed588a3b2456008875d81a33b
canhtrinh <canh.coding@gmail.com>
2016-11-08 15:22:06 -0500

    fix: amended index.js to expand store

--- client/src/app.js
+++ client/src/app.js
@@ -1,59 +1,86 @@
+// import React from 'react';
+// import ChatForm from './chatForm.js';
+// import ChatBody from './ChatBody.js';
+
+// let socket = io();
+
+// class PrimaryChatroom extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentChat: []
+//     };
+//     this.handleReceiveMessage = this.handleReceiveMessage.bind(this);
+//   }
+
+//   componentDidMount() {
+//     let that = this;
+//     socket.on('chat message', function(message){
+//       that.handleReceiveMessage(message);
+//     });
+//     socket.on('disconnect', function(message){
+//       that.handleReceiveMessage(message);
+//     });
+//   }
+
+//   handleReceiveMessage(chat) {
+//     let newChat = this.state.currentChat.concat(chat);
+//     this.setState({
+//       currentChat: newChat
+//     });
+//   }
+
+//   socketEmitMessage(chat) {
+//     socket.emit('chat message', chat);
+//   }
+
+//   render() {
+
+//     let chatMap = this.state.currentChat.map((msg, i) => 
+//       <ChatBody message={msg} key={i} />
+//     )
+
+//     return (
+//       <div>
+
+//         <ul id="messages">
+//         {chatMap}
+//         </ul>
+
+//         <ChatForm 
+//           handleSearchInputChange={this.socketEmitMessage.bind(this)}
+//         />
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default PrimaryChatroom
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import ChatForm from './chatForm.js';
-import ChatBody from './ChatBody.js';
+import MessageList from './ChatBody.js';
+import Message from './Message';
 
 let socket = io();
 
-class PrimaryChatroom extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentChat: []
-    };
-    this.handleReceiveMessage = this.handleReceiveMessage.bind(this);
-  }
+const App = () => (
+  <div>
+    <MessageList />
+    <ChatForm />
+    <Message />
+  </div>
+);
 
-  componentDidMount() {
-    let that = this;
-    socket.on('chat message', function(message){
-      that.handleReceiveMessage(message);
-    });
-    socket.on('disconnect', function(message){
-      that.handleReceiveMessage(message);
-    });
-  }
-
-  handleReceiveMessage(chat) {
-    let newChat = this.state.currentChat.concat(chat);
-    this.setState({
-      currentChat: newChat
-    });
-  }
-
-  socketEmitMessage(chat) {
-    socket.emit('chat message', chat);
-  }
-
-  render() {
-
-    let chatMap = this.state.currentChat.map((msg, i) => 
-      <ChatBody message={msg} key={i} />
-    )
-
-    return (
-      <div>
-
-        <ul id="messages">
-        {chatMap}
-        </ul>
-
-        <ChatForm 
-          handleSearchInputChange={this.socketEmitMessage.bind(this)}
-        />
-
-      </div>
-    );
-  }
-}
-
-export default PrimaryChatroom
+export default App;
