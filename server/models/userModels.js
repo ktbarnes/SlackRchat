b56cef4e8ca327aba315318d7472486bf8143bbb
ktbarnes <katosych@gmail.com>
2016-11-08 15:33:28 -0500

    feat(auth): can validate password and generate jwt
    basics of backend ser authorization work with /login endpoint and jwt sent upon successfull password
    
    comparison

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,17 +1,14 @@
-
 var database = require('../db/db');
 
 var User = {
   getUsers: function(){
-    return database.select('*').from('users');
+    return database.select('*').from('users').then(data => {
+      console.log(data);
+      return data;
+    });
   },
   getUser: function(email) {
-    console.log('inside userModels email ', JSON.stringify(email));
-    // email = JSON.stringify(email);
-    var stuff = database.select('*').from('users').where({'email': email});
-    console.log(stuff);
-    return stuff;
-    // return database('users').where('email', JSON.stringify(email));
+    return database('users').where({'email': email});
   },
   postUser: function(data){
     return database('users')
