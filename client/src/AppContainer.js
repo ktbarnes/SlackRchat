152515143b9995a0f1b2e04b1c37c3601458b280
canhtrinh <canh.coding@gmail.com>
2016-11-10 23:06:26 -0500

    refactor: minor cleanup

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -1,9 +1,4 @@
 import React, { PropTypes } from 'react';
-import ChatForm from './chatForm.js';
-import MessageList from './ChatBody.js';
-import Message from './Message.js';
-import ChatReducer from '../reducers/ChatReducer.js';
-import { addMessage } from '../actions/ChatActions';
 import Nav from './nav.js';
 import PrimaryChatroom from './PrimaryChatroom.js';
 import SideBar from './sidebar.js';
@@ -11,7 +6,6 @@ import SideBar from './sidebar.js';
 class AppContainer extends React.Component {
 
   render(){
-    const { dataStore } = this.props
     return (
       <div>
 
