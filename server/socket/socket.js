058c13251b28f4bffca91ba1b156e68fa856905e
canhtrinh <canh.coding@gmail.com>
2016-11-11 10:45:08 -0500

    feat: added socket namespaces and rooms on the server side. working on client side rendering chat me

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -25,13 +25,17 @@
 
 module.exports = function(socket){
 
+  var currentRoom = '';
+
   console.log('a user connected to:' + socket.id);
   socket.broadcast.emit('someoneJoin','A user connected');
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    socket.emit('chat message', msg);
-    socket.broadcast.emit('chat message', msg);
+  socket.on('chat message', function(fromClient){
+    console.log('chat message: ' + fromClient.msg);
+    console.log('room name: ' + fromClient.room);
+    // socket.emit('chat message', fromClient.msg);
+    // socket.broadcast.emit('chat message', fromClient.msg);
+    socket.to(fromClient.room).emit('chat message', fromClient.msg);
   });
 
   socket.on('disconnect', function(){
@@ -41,10 +45,18 @@ module.exports = function(socket){
   });
 
 
-  // io.on('chat message', function(msg){
-  //   console.log('message: ' + msg);
-  //   socket.emit('chat message', msg);
-  // });
+  //Room-specific code
 
+  socket.on('changeRoom', function(room) {
+    currentRoom = room;
+    socket.join(room);
+    console.log("currentRoom",currentRoom);
+    socket.in(currentRoom).broadcast.emit('roomMessage', 'what is going on, party people?');
+    socket.to(currentRoom).emit('roomMessageTest', 'TEST - what is going on, party people?');
+  });
+
+  // io.sockets.in(currentRoom).emit('message', 'what is going on, party people?');
+  socket.in(currentRoom).broadcast.emit('roomMessage', 'what is going on, party people?');
+  socket.to(currentRoom).emit('roomMessageTest', 'TEST - what is going on, party people?');
     
 }
