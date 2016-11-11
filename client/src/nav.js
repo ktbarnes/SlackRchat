02cb5681e36d9e6aa4a3e0ddff2eb805ef8ac7f2
Janis Randall <juliafrandall@gmail.com>
2016-11-11 18:53:06 -0500

    feat(profile modal):
    profile modal rendering

--- client/src/nav.js
+++ client/src/nav.js
@@ -1,20 +1,50 @@
 import React from 'react'
-import Logout from "./logout.js"
+import Logout from './logout.js'
 import { loginUser } from '../actions/loginActions'
 import { logoutUser } from '../actions/logoutActions'
 import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router'
+import Profile from './profile.js'
+import { Modal, 
+        Button,
+        ModalHeader,
+        ModalTitle,
+        ModalFooter,
+        ModalBody
+         } from 'react-bootstrap' 
 
 export default class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModel: false
+    }
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+    open() {
+      console.log('inside open')
+      this.setState({
+        showModel: true
+      })
+    };
+
+    close() {
+      this.setState({
+        showModel: false
+      })
+    };
 
   render() {
     return (
       <div className="nav">
         <h1 className="title">Slacker</h1>
-        <a className="navbutton" href="#" onClick={this.open}>Profile</a>
+        <a className="navbutton" href="#" role="button" 
+        onClick={this.open}>Profile</a>
+        <Profile show={this.state.showModel} onHide={this.close}/>
         <a className="navbutton" href="/Login"> Login </a>
         <a className="navbutton" href="/Logout">LogOut</a>
       </div>
-    );
+    )
   }
 }
   
--- client/src/profile.js
+++ client/src/profile.js
@@ -1,134 +1,58 @@
 //profile page....
 //bootstrap modal
-import {Modal, 
-        ModalHeader, 
-        ModalTitle, 
-        ModalClose, 
-        ModalBody} from 'react-modal-bootstrap'
+import { Modal, 
+        Button,
+        ModalHeader,
+        ModalTitle,
+        ModalFooter,
+        ModalBody
+         } from 'react-bootstrap'        
 import React from 'react';
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
+export default class Profile extends React.Component {
 
-    // this.state= {
-    // 	first: '',
-    // 	last: '',
-    // 	phone: '',
-    // 	email: '',
-    // 	photo:
-    // 	//these might be filled in initially in signup
-    // 	this.handleSubmit = this.handleSubmit.bind(this);
+    // constructor(props) {
+    //   super(props)
+      
+    //   this.state = {
+    //     showModel: false
+    //   }
+     
+    //   open = () => {
+    //     this.setState({
+    //       showModel: true
+    //     });
+    //   }
+
+    //   close = () => {
+    //     this.setState({
+    //       showModel: false
+    //     })
+    //   }
+    
     // }
+    render(){
+    return (
+      <Modal id="profile_modal" show={this.props.show}>
+        <Modal.Header>
+          <Modal.Title id="modal_header">Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body id="modal_content">
+          <p>Content</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn btn-default" onClick={this.props.onHide}>Close</Button>
+          <Button className="btn btn-default" onClick={this.props.onHide}>Save</Button>
+        </Modal.Footer>
 
-    // handleInputChange(e) {
-    // 	this.setState({
-    // 		first: e.target.first,
-    // 		last: e.target.last,
-    // 		phone: e.target.phone,
-    // 		email: e.target.email,
-    // 		photo: 
-    // 	})
-    // }
-    this.state = {
-      isOpen: false
-    };
-  
-  openModal = () => {
-  	this.setState({
-  	  isOpen: true
-  	});
-  };
-
-  hideModal = () => {
-  	this.setState({
-  	  isOpen: false
-  	});
-  };
-
-
-  render() {
-  	return (
-  	  <div>
-  		<h2>Edit your profile</h2>
-  	  </div>
-  	  <div>
-  	  	<h3>Profile Picture</h3>
-  		<input type='file' accept='image/*' 
-  		 data-action='profilepicture' />
-  	  </div>
-  	  <div>
-  	  	<label for='first_name'>First Name</label>
-  	  		<input type='text' name='first_name' placeholder='First Name' value={this.state.first} 
-  	  		onChange={this.handleInputChange.bind(this)} />
-  	  	<label for='last_name'>Last Name</label>
-  	  		<input type='text' name='last_name' placeholder='Last Name' value={this.state.last} 
-  	  		onChange={this.handleInputChange.bind(this)} />
-  	  	<label for='phone_number'>Phone Number</label>
-  	  		<input type='text' name='phone_number' placeholder='Phone Number' value={this.state.value} />	
-  	  
-  	  	<button onClick={this.handleSubmit}>Submit Profile </button>
-  	  	//will this submit all fields?
-  	  </div>
-    )
+      </Modal>
+     ) 
   }
 }
 
 
-  //   this.state = {
-  //     isOpen: false
-  //   };
-  
-  // openModal = () => {
-  // 	this.setState({
-  // 	  isOpen: true
-  // 	});
-  // };
 
-  // hideModal = () => {
-  // 	this.setState({
-  // 	  isOpen: false
-  // 	});
-  // };
-
-  //versus
-  NAVBAR COMPONENT
-  getInitialState(){
-  	return {showModal: false};
-  },
-  close(){
-  	this.setState({showModal:false});
-  },
-  open(){
-  	this.setState({showModal:true});
-  }
-  render(){
-  	return (
-  	  <div className='nav'>
-  	  <h1>Slacker<h1>
-  	  <a className='btn btn-primary'
-  	  	href='#'
-  	  	onClick={this.open}>Profile
-  	  </a>
-  	  <a className='btn btn-primary'
-  	  	href='/Signin'>Signin
-  	  </a>
-  	  <a className='btn btn-primary'
-  	  	href='Logout'>Logout
-  	  </a>
-  	  <Modal show={this.state.showModal} onHide={this.close} />
-  	  </div>				
-  		)
-  }
-
-
-// <button type="button" className="close" onClick{this.props.onHide}></button>
-//   	  	<div>Profile Information from User will Go Here</div>
-
-
-
-
-
+//Need to change Button Save onClick functionality
 
 
 
