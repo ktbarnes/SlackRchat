72d257ecaf9cadf321171d9ecbf66b1ceff9ddc9
ktbarnes <katosych@gmail.com>
2016-11-11 20:16:49 -0500

    feat(auth): redirect to login if not authenticated, if login in is successful redirect to slacker
    authorization and redirecting is working on a basic level

--- client/routes.js
+++ client/routes.js
@@ -7,10 +7,22 @@ import Master from './src/master.js'
 import Login from './src/login.js'
 import Logout from './src/logout.js'
 
+function loggedIn() {
+  return !!localStorage.id_token;
+}
+
+function requireAuth(nextState, replace) {
+  if(!loggedIn()) {
+    replace({
+      pathname: '/login'
+    })
+  }
+}
+
 export default (
   <Route path="/" component={Master}>
-    <IndexRoute component={AppContainer} />
-    <Route path="Login" component={Login} />
-    <Route path="Logout" component={Logout} />
+    <IndexRoute component={AppContainer} onEnter={requireAuth} />
+    <Route path="login" component={Login} />
+    <Route path="logout" component={Logout} />
   </Route>
 )
