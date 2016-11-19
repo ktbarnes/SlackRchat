38db3970c28bde63d96c119b58576a060c53b4bc
Janis Randall <juliafrandall@gmail.com>
2016-11-19 15:50:08 -0500

    refactor(other profile): changed flow of data

--- client/src/OtherUserProfile.js
+++ client/src/OtherUserProfile.js
@@ -7,67 +7,128 @@
          } from 'react-bootstrap'        
 import React from 'react'
 import { dispatch, connect } from 'react-redux';
+import axios from 'axios'
 
 
-const OtherUserProfile = ({clickedUser}) => {
+// const OtherUserProfile =({clickedUser})
+
+class OtherUserProfile extends React.Component {
+  constructor(props) {
+// const OtherUserProfile =({clickedUser}) => {
+    super(props)
+    this.state = {
+      first: ''
+    }
+    // let getID = clickedUser.id
+    // console.log(clickedUser, 'this is the clicedd')
+  
+  // console.log(clickedUser, 'this is the clicedd')
+  
+  let getID = this.props.clickedUser.id
+  console.log(getID,"this is the iddddd")
+
+     axios.post('db/getOther', {id: getID})
+     .then((response)=> {
+      // console.log(response, 'this is the new resposne')
+      this.setState({
+        id: response.data[0].id,
+        email: response.data[0].email,  
+        first: response.data[0].first,
+        about: response.data[0].about,
+        first: response.data[0].first,
+        last: response.data[0].last,
+        github: response.data[0].github,
+        facebook: response.data[0].facebook,
+        twitter: response.data[0].twitter,
+        linkedin: response.data[0].linkedin
+      })
+    //   console.log(response.data[0].first, 'this is the response.....')
+    //   this.setState({
+    //     first: response.data[0].first
+    //   })
+    // })
+  }) 
+}
+    //from channel_messages
+    // axios.get('/db/messages')
+    // .then(res => {
+    //   res.data.forEach(msg => {
+    //     let eachMsg = {
+    //       id: msg.id,
+    //       username: msg.username,
+    //       userIDinDB: msg.userIDinDB,
+    //       channelName: msg.channelName,
+    //       channelIDinDB: msg.channelIDinDB,
+    //       text: msg.message,
+    //       created_at: msg.created_at
+    //     }
+    //     this.handleReceive(addMessageFromDB,eachMsg);
+    //   });
+    // });
+
+    render(){
     return (
       <div className='table'>
         <div>
-          <h2 className='tableUsername'>{clickedUser.username}</h2>
+          <h2 className='tableUsername'>{this.state.username}</h2>
         </div>
-      <div>
-        <h3>Profile Picture</h3>
-      </div>
+        <div>
+          <h3>Profile Picture</h3>
+        </div>
       <table className='tableOtherUserInformation'>
         <tbody>
           <tr>
             <td className='profileHeading'>First Name</td>
-            <td>{clickedUser.first}</td>
+            <td>{this.state.first}</td>
           </tr>
           <tr>
             <td>Last Name</td>
-            <td>{clickedUser.last}</td>
+            <td>{this.state.last}</td>
           </tr>
           <tr>
             <td>Email</td>
-            <td>{clickedUser.email}</td>
+            <td>{this.state.email}</td>
           </tr>
           <tr>
             <td>Phone Number</td>
-            <td>{clickedUser.phone}</td>
+            <td>{this.state.phone}</td>
           </tr>
           <tr>
             <td>Github</td>
-            <td>{clickedUser.github}</td>
+            <td>{this.state.github}</td>
           </tr>
           <tr>
             <td>Linkedin</td>
-            <td>{clickedUser.linkedin}</td>
+            <td>{this.state.linkedin}</td>
           </tr>
           <tr>
             <td>Facebook</td>
-            <td>{clickedUser.facebook}</td>
+            <td>{this.state.facebook}</td>
           </tr>
           <tr>
             <td>Twitter</td>
-            <td>{clickedUser.twitter}</td>
+            <td>{this.state.twitter}</td>
           </tr>
           <tr>
             <td>About Me</td>
-            <td>{clickedUser.about}</td>
+            <td>{this.state.about}</td>
           </tr>
         </tbody>   
       </table> 
-      <button>Close</button> 
+      <button>Close</button>
       </div>  
      ) 
   }
+}
 
 const mapStateToProps = (state, ownProps) => {
+  console.log("in otheruserprofile",state.allReducers.ClickedUserProfileReducer)
 	return {
+    // console.log(clickedUser, 'this is my clickeddd user')
 		// allUsers: state.allReducers.UserReducer,
     clickedUser: state.allReducers.ClickedUserProfileReducer
 	}
+  // console.log(clickedUser, 'this is my clickeddd user')
 }
 
 export default connect(mapStateToProps)(OtherUserProfile)
