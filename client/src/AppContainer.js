2643de7d351907b67543c96d568638cbf4e4f232
canhtrinh <canh.coding@gmail.com>
2016-11-24 11:40:38 -0500

    docs: added documentation for AppContainer. not still a work in progress

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -16,35 +16,71 @@ import { setCurrentRoom } from '../actions/CurrentRoomActions';
 import { default as Fade } from 'react-fade';
 import { Grid, Row, Col } from 'react-bootstrap';
 
+
+/*
+Note to user: 
+This file is the primary container for the application. 
+What's rendered is four components:
+  - TopNav, which is the nav bar for the application
+  - LeftSideBar (which contains the list of all public channels, a user's subscribed channels, and user's DMs)
+  - PrimaryChatroom, which is a separate component that houses the body of the chatroom (i.e. MessageList) 
+    and the input form fields (i.e. ChatForm)
+  - RightSideBar. Note that the sidebar is an npm module. The module required a parent component,
+    in this case AppContainer. All of the components above were wrapped in the SideBar component rendered below
+Props being passed in from the Redux store:
+    rooms: state.allReducers.RoomReducer -- all available channels
+    currentUser: state.allReducers.CurrentUserReducer -- the store's tracker for who the "current user" is
+    DMRooms: state.allReducers.DMRoomReducer, -- all available direct message rooms
+    currentRoom: state.allReducers.CurrentRoomReducer -- the store's tracker for which room the current user is currently in
+*/
+
+
 class AppContainer extends React.Component {
 
   constructor(props){
     super(props)
-    this.socket = io('/Hack-Reactor-NameSpace');
+    this.socket = io('/Hack-Reactor-NameSpace'); //instantiates namespace for socket
     this.state = {
       docked: false,
       open: false,
       transitions: true,
       shadow: true,
       pullRight: true
-    }
+    } //state initialized for purpose of RightSideBar
     this.toggleUserDock = this.toggleUserDock.bind(this)
   }
 
+  //Following set of functions used for RightSideBar
   onSetOpen(open) {this.setState({open: open})}
   menuButtonClick(ev) {
     ev.preventDefault();
     this.onSetOpen(!this.state.open);
-  }
+  } 
   toggleUserDock(ev){this.setState({ docked: !this.state.docked })}
+  //End of RightSideBar functions
 
+  //ComponentDidMount initializes the application for the user by downloading stored data from the database
+  //First, it downloads all users, then all of the user's direct message rooms, then all of the public channels
+  //It then initializes the socket.io receiver. This is where all the socket listeners are defined
   componentDidMount() {
-    this.downloadAllUsers();
-    this.downloadAllDMRooms();
-    this.downloadChannels();
+    this.downloadAllUsers(); //download all users on the application
+    this.downloadAllDMRooms(); //download all direct message rooms
+    this.downloadChannels(); //download all channels
+
+    /*
+    Socket listener for incoming live chat messages
+    Includes events for:
+      - chat message (live incoming chat message within a channel/DM room)
+      - disconnected (notification that a user has signed out of the application)
+      - someoneJoin (notification that someone has joined the application)
+      - direct message (live incoming direct message from another user)
+      - connect (what socket should first do upon connect, which is to "changeRoom" to 
+        current room and get a list of all "online" users from the socket)
+      - online / offline toggle - tracks login and logout events for other users
+    */
+
     this.socket.on('chat message', 
       incoming => {
-        console.log("CHAT MESSAGE", incoming)
 
         //add message to ChatReducer
         this.handleReceive(addMessageFromSocket,{
@@ -58,56 +94,54 @@ class AppContainer extends React.Component {
 
         //increment unread messages only if user is not in that room
         if(incoming.channelName !== this.props.currentRoom.channelName){
-          //code
-          console.log("incoming.channelName",incoming.channelName)
-          console.log("currentRoom.channelName",this.props.currentRoom.channelName)
           this.handleReceive(incrementUnreadMessageCounter,incoming.channelName)
         }
       }
-    );
+    ); //end of this.socket.on('chat message')
 
     this.socket.on('disconnected', txt => this.handleReceive(addMessageFromSocket,{text: txt}) );
     
     this.socket.on('someoneJoin', txt => this.handleReceive(addMessageFromSocket,{text: txt}) );
     
+    //This event may require some explanation...:
     this.socket.on("direct message", incoming => {
-      console.log("trying to get a room", incoming.room);
-      window.alert(incoming.msg)
+      window.alert(incoming.msg) //notification to user that another user is trying to send a direct message
+
+      //following code is used because it needs to track whether current user has ever had a direct message with other user
+      //if not you add that room to the DMRoomReducer. otherwise, you just go straight to that room
       this.roomExists = false;
       for (let i = 0; i<this.props.DMRooms.length; i++){
-        console.log("comparison of room IDs in AppCotainer line 87",incoming.room.id," ",this.props.DMRooms[i].id)
-        if(incoming.room.id === this.props.DMRooms[i].id){
-          console.log("inside if statement for direct messages")
-          this.roomExists = true;
-        }
+        if(incoming.room.id === this.props.DMRooms[i].id){ this.roomExists = true; }
       }
-      if(!this.roomExists){ 
-        console.log("adding a DM room in AppContainer while being direct messaged")
-        this.handleReceive(addDMRoomFromSocket,incoming.room); }
+
+      //if that room does not exist, this is where the action is dispatched to the DMRoomReducer
+      if(!this.roomExists){ this.handleReceive(addDMRoomFromSocket,incoming.room); }
+
+      //In either case, must emit to socket a change of the room to the requested room from the oother user, and then
+      //reset the store's current room to that room
       this.socket.emit('changeRoom', incoming.room.channelName)
       this.handleReceive(setCurrentRoom,incoming.room);
-    });
+    }); //End of this.socket.on(direct message)
     
     this.socket.on('connect', (txt) => {
       this.socket.emit('changeRoom', this.props.currentRoom.channelName);
-      // console.log(this.props.currentUser.username,"WHO AM I???? AppContainer line 83")
       this.socket.emit("getAllLoggedInUsersFromSocket")
     }); 
     
     this.socket.on('onlineToggle ON', email => {
-      console.log("who just logged in",email);
+      // console.log("who just logged in",email);
       this.handleReceive(toggleOnlineUser,email);
     });
     
     this.socket.on('onlineToggle OFF', email => {
-      console.log("who just logged off",email);
+      // console.log("who just logged off",email);
       this.handleReceive(toggleOfflineUser,email);
     });
     
     this.socket.on("getAllLoggedInUsersFromSocket", onlineUsersObj => {
-      console.log("TRYING TO GET A MESSAGE")
-      console.log("online users Obj",onlineUsersObj)
-      console.log("online users Obj keys",Object.keys(onlineUsersObj))
+      // console.log("TRYING TO GET A MESSAGE")
+      // console.log("online users Obj",onlineUsersObj)
+      // console.log("online users Obj keys",Object.keys(onlineUsersObj))
       if(Object.keys(onlineUsersObj).length > 0){
         Object.keys(onlineUsersObj).forEach( (email) =>
           this.handleReceive(downloadOnlineUsers,email)
@@ -150,7 +184,6 @@ class AppContainer extends React.Component {
     .then( () => {
       axios.get('/db/channels')
       .then( (res) => {
-        // console.log("downloadAllChannels in AppContainer",res.data)
         res.data.forEach( (msg) => {
           let eachRoom = {
             id: msg.id,
@@ -179,15 +212,12 @@ class AppContainer extends React.Component {
     this.currentUserIDfromDB;
     axios.get('/db/getMe', { headers: { "authorization": "Bearer " + localStorage.getItem('id_token')}})
     .then(res => {
-      console.log("CANH IS HERE in AppContainer",res.data)
       this.props.dispatch(setCurrentUser(res.data));
-      // console.log('supposedly set the curretUser ',this.props.currentUser);
+
       //now download all users
       axios.get('/db/users')
       .then( (resp) => {
-        // console.log("what comes in from the DB?",resp)
         resp.data.forEach( (person) => {
-          // console.log(person.phone, "this is the persons phone number")
           let eachUser = {
             id: person.id,
             username: person.username,
@@ -203,11 +233,8 @@ class AppContainer extends React.Component {
             onlineToggle: false
           }
           this.handleReceive(addUser,eachUser);
-          // if(this.currentUserIDfromDB === person.id){
-          //   this.handleReceive(setCurrentUser,eachUser);
-          // }
         });
-        // console.log("email email email???",this.props.currentUser.email);
+
         this.socket.emit("setMyEmailInSocket",{
           email: this.props.currentUser.email,
           username: this.props.currentUser.username
@@ -224,11 +251,9 @@ class AppContainer extends React.Component {
     axios.get('/db/getMe',
     { headers: { "authorization": "Bearer "+localStorage.getItem('id_token') }})
     .then( (res) => {
-      // console.log("who is my user???",res.data)
       this.currentUserIDfromDB = res.data.id;
       axios.get('/db/DMRooms')
       .then( (res) => {
-        // console.log("what are the DM rooms that are downloaded",res.data)
         res.data.forEach( (msg) => {
           let eachRoom = {
             id: msg.id,
@@ -240,7 +265,6 @@ class AppContainer extends React.Component {
             aliasName: msg.aliasName,
             currentRoomToggle: (this.currentRoom === msg.channelName)
           } 
-          // console.log(this.currentUserIDfromDB, "   ", eachRoom.user1ID, "   ", eachRoom.user2ID)
           if(this.currentUserIDfromDB === eachRoom.user1ID || this.currentUserIDfromDB === eachRoom.user2ID){
             this.handleReceive(addDMRoom,eachRoom);
           }
@@ -264,8 +288,6 @@ class AppContainer extends React.Component {
     };
 
     return (
-
-
       <Sidebar {...sidebarProps} className='RightSideBar'>
           <TopNav />          
           <Grid id="Container">
@@ -289,7 +311,6 @@ class AppContainer extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  // console.log("current room",state.allReducers.CurrentRoomReducer)
   return { 
     rooms: state.allReducers.RoomReducer,
     currentUser: state.allReducers.CurrentUserReducer,
