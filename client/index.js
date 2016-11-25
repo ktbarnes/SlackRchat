da923f9532535cd9c79554c1e47ba8691855bece
canhtrinh <canh.coding@gmail.com>
2016-11-25 16:32:13 -0500

    docs: added documentation for index.js and cleaned up file

--- client/index.js
+++ client/index.js
@@ -7,35 +7,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import allReducers from './reducers/index'
 import routes from './routes.js'
 
+/*
+Note to reader:
+This is the primary entry point for the application. It 
+instantiates the Redux store and sets up the React Router
 
-// const logger = store => next => action => {
-//   console.log('dispatching', action)
-//   let result = next(action)
-//   console.log('next state', store.getState())
-//   return result
-// }
-
-// const crashReporter = store => next => action => {
-//   try {
-//     return next(action)
-//   } catch (err) {
-//     console.error('Caught an exception!', err)
-//     Raven.captureException(err, {
-//       extra: {
-//         action,
-//         state: store.getState()
-//       }
-//     })
-//     throw err
-//   }
-// }
+Routes are listed in routes.js
+*/
 
 const store = createStore(
   combineReducers({
     allReducers,
     routing: routerReducer
-  }),
-  // applyMiddleware(logger, crashReporter)
+  })
 )
 
 const history = syncHistoryWithStore(browserHistory, store)
