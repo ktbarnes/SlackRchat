733e32975ad5e2fe2a535104c489f1a8f10902fd
ktbarnes <katosych@gmail.com>
2016-11-26 16:27:00 -0500

    feat(analytics): more editing

--- client/src/Analytics.js
+++ client/src/Analytics.js
@@ -80,12 +80,6 @@ class Analytics extends React.Component {
       channel.datasets[0].data = Object.values(channels);
     })
     .catch(error =>  props.router.replace('/'));
-
-    // axios.get('/db/getMe',
-    // { headers: { "authorization": "Bearer "+localStorage.getItem('id_token') }})
-    // .then(res => {
-    //   this.props.dispatch(setCurrentUser(res.data));
-    // })
   }
 
   onSelect(updatedValue) {
@@ -105,7 +99,6 @@ class Analytics extends React.Component {
   }
 
   render() {
-    console.log('what are the current props on this render ', this.props)
     return (
       <Fade duration={.2}>
         <TopNav />
@@ -130,7 +123,6 @@ class Analytics extends React.Component {
   }
 
 }
-
 
 const mapStateToProps = (state, ownProps) => {
   return { 
